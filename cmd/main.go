12965f7fd41858e76c5d72a0f15aec2c261a0f6e
cyp0633 <cyp0633@gmail.com>
2022-09-06 16:45:47 +0800

    Get user type text in a more elegant way

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,7 @@ func auth() {
 	fmt.Printf("请输入用户名和密码\n")
 	fmt.Scanf("%s %s\n", &username, &password)
 	if middleware.Auth(username, password) {
-		fmt.Printf("当前选择为%v出题\n", models.AccountTypeText[models.CurrentUser.AccountType]) // 根据当前用户类型选择教育等级名称
+		fmt.Printf("当前选择为%v出题\n", models.CurrentUser.GetUserTypeText()) // 根据当前用户类型选择教育等级名称
 	} else {
 		fmt.Printf("请输入正确的用户名、密码\n")
 		auth() // 使用递归重新验证。Go 的递归深度是无限的，所以没有问题
@@ -28,23 +28,24 @@ func auth() {
 
 // inputHandler 用于处理输入，并调用相应的函数
 func inputHandler() {
+	regex, err := regexp.Compile(`切换为..`)
+	if err != nil {
+		log.Printf("正则表达式匹配错误：%v", err)
+	}
 	for {
-		fmt.Printf("准备生成%v数学题目，请输入生成题目数量（输入-1将退出当前用户，重新登录）：\n", models.AccountTypeText[models.CurrentUser.AccountType])
+		fmt.Printf("准备生成%v数学题目，请输入生成题目数量（输入-1将退出当前用户，重新登录）：\n", models.CurrentUser.GetUserTypeText())
 		var num int
 		var tmp string
 		fmt.Scanf("%s\n", &tmp)
-		matchChangeType, err := regexp.MatchString(`切换为..`, tmp) // 使用正则表达式判断是否切换用户类型
-		if err != nil {
-			log.Printf("正则表达式匹配错误：%v", err)
-		}
-		if matchChangeType { // 切换用户类型
+		matchChangeType := regex.MatchString(tmp) // 使用正则表达式判断是否切换用户类型
+		if matchChangeType {                      // 切换用户类型
 			ok := models.ChangeUserType(tmp[4:])
 			for !ok {
 				fmt.Printf("请输入小学、初中和高中三个选项中的一个\n")
 				fmt.Scanf("%s\n", &tmp)
 				ok = models.ChangeUserType(tmp)
 			}
-			fmt.Printf("准备生成XX数学题目，请输入生成题目数量\n")
+			fmt.Printf("准备生成%v数学题目，请输入生成题目数量\n", models.CurrentUser.GetUserTypeText())
 		}
 		fmt.Sscanf(tmp, "%d", &num)
 		if num == -1 {
@@ -54,8 +55,9 @@ func inputHandler() {
 		}
 		if num > 30 || num < 10 {
 			fmt.Printf("题目数量不合法")
+			continue
 		}
-		return // TODO: 生成题目
+		middleware.GetProblems(num)
 	}
 }
 
