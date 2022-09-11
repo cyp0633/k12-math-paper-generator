3c2caffc4ce996d80f7e245a16f6c84af2f0511d
cyp0633 <cyp0633@gmail.com>
2022-09-11 15:44:43 +0800

    Add message sender

---
+++ internal/models/sms.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials"
+	dysmsapi "github.com/aliyun/alibaba-cloud-sdk-go/services/dysmsapi"
+)
+
+// SendSms 用于向客户端发送验证码短信，返回是否成功。
+func SendSms(phone string, code int) bool {
+	config := sdk.NewConfig()
+	credentials := credentials.NewAccessKeyCredential("accessKeyId", "accessKeySecret") // to be filled later
+	client, err := dysmsapi.NewClientWithOptions("cn-hangzhou", config, credentials)
+	if err != nil {
+		log.Printf("创建短信客户端失败：%v", err)
+		return false
+	}
+	request := dysmsapi.CreateSendSmsRequest()
+	request.Scheme = "https"
+	request.PhoneNumbers = phone
+	request.TemplateParam = "{\"code\":\"" + fmt.Sprint(phone) + "\"}"
+	request.SignName = "数学学习软件"
+	request.TemplateCode = "SMS_251096882"
+	response, err := client.SendSms(request)
+	if err != nil {
+		log.Printf("发送短信失败：%v", err)
+		return false
+	}
+	log.Printf("response is %#v\n", response)
+	return true
+}
