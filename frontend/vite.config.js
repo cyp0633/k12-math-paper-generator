c69b1b3246291eae094860e6af76cfb8b2b192eb
cyp0633 <cyp0633@gmail.com>
2022-09-15 20:14:45 +0800

    Won't clean dist directory while building frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: false,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
